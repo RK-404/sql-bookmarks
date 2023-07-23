ec323eeb27957b4b2ed4d6d3401823b3bdf4ec93
RK-404 <rizwan_k@RIZWANs-MacBook-Air.local>
2023-07-23 12:59:21 -0400

    Added Delete and Update

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const bookmarks = express.Router();
-const { checkName, checkBoolean } = require("../validations/checkBookmarks.js");
-const { getAllBookmarks, getBookmark, createBookmark } = require("../queries/bookmarks");
+const { checkName, checkBoolean, validateURL } = require("../validations/checkBookmarks.js");
+const {
+    getAllBookmarks,
+    getBookmark,
+    createBookmark,
+    deleteBookmark,
+    updateBookmark
+} = require("../queries/bookmarks");
 
 // INDEX
 bookmarks.get("/", async (req, res) => {
@@ -26,7 +32,7 @@ bookmarks.get("/:id", async (req, res) => {
 });
 
 // CREATE
-bookmarks.post("/", checkBoolean, checkName, async (req, res) => {
+bookmarks.post("/", checkBoolean, checkName, validateURL, async (req, res) => {
     try {
         const bookmark = await createBookmark(req.body);
         res.json(bookmark);
@@ -34,6 +40,25 @@ bookmarks.post("/", checkBoolean, checkName, async (req, res) => {
     catch (error) {
         res.status(400).json({ error: error });
     }
+});
+
+// DELETE
+bookmarks.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    const deletedBookmark = await deleteBookmark(id);
+    if (deletedBookmark.id) {
+        res.status(200).json(deletedBookmark);
+    }
+    else {
+        res.status(404).json("Bookmark not found");
+    }
+});
+
+// UPDATE
+bookmarks.put("/:id", checkBoolean, checkName, validateURL, async (req, res) => {
+    const { id } = req.params;
+    const updatedBookmark = await updateBookmark(id, req.body);
+    res.status(200).json(updatedBookmark);
 });
 
 module.exports = bookmarks;
--- validations/checkBookmarks.js
+++ validations/checkBookmarks.js
@@ -9,16 +9,23 @@ const checkName = (req, res, next) => {
 
 const checkBoolean = (req, res, next) => {
     const { is_favorite } = req.body;
-    if (
-      is_favorite == "true" ||
-      is_favorite == "false" ||
-      is_favorite == undefined
-    ) {
+    if (["true", "false", true, false, undefined].includes(is_favorite)) {
         next();
     }
     else {
         res.status(400).json({ error: "is_favorite must be a boolean value" });
     }
-};  
-      
-module.exports = { checkBoolean, checkName };
+};
+
+const validateURL = (req, res, next) => {
+    const { url } = req.body;
+    if (url.substring(0, 7) === "http://" || url.substring(0, 8) === "https://") {
+        next();
+    }
+    else {
+        res.status(400)
+        .json({ error: `You forgot to start your url with http:// or https://` });
+    }
+};
+
+module.exports = { checkBoolean, checkName, validateURL };
